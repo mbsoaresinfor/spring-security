5dad22a4ace666d959109bfba6bb515d9c32b0bc
marcelo soares <mbsoares.infor@gmail.com>
2023-05-17 22:06:23 -0300

    cont. project.

--- src/main/java/br/com/security/SpringSecurityApplication.java
+++ src/main/java/br/com/security/SpringSecurityApplication.java
@@ -1,9 +1,11 @@
-package br.com.security.springsecurity;
+package br.com.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class SpringSecurityApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/security/controller/ProductController.java
+++ src/main/java/br/com/security/controller/ProductController.java
@@ -1,4 +1,4 @@
-package br.com.security.springsecurity.controller;
+package br.com.security.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
